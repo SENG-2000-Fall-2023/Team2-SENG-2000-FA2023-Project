c3b5cf83477172febd5af53867e8ae840615ab32
Jeremiah Teele <teelej18@students.ecu.edu>
2023-11-30 18:04:13 -0500

    Added formatting for names and grades

--- Src/GradeBook.java
+++ Src/GradeBook.java
@@ -11,11 +11,13 @@ public class GradeBook {
     }
 
     public void addStudent(String name) {
+        name = capitalizeFirstLetter(name);
         students.put(name, new Student(name));
         studentOrder.add(name);
     }
 
     public void removeStudent(String name) throws Exception {
+        name = capitalizeFirstLetter(name);
         if (!students.containsKey(name)) {
             throw new Exception("Student not found");
         }
@@ -23,14 +25,29 @@ public class GradeBook {
         studentOrder.remove(name);
     }
 
+    public String capitalizeFirstLetter(String word) {
+        word = word.toLowerCase();
+        word = word.substring(0,1).toUpperCase() + word.substring(1);
+        return word;
+    }
+
     public void addGrade(String name, String subject, int grade) throws Exception {
+        name = capitalizeFirstLetter(name);
+        subject = capitalizeFirstLetter(subject);
         if (!students.containsKey(name)) {
             throw new Exception("Student not found");
         }
+
+        if (grade < 0 || grade > 100) {
+            throw new Exception("Invalid grade entered");
+        }
+
         students.get(name).addGrade(subject, grade);
     }
 
     public int getGrade(String name, String subject) throws Exception {
+        name = capitalizeFirstLetter(name);
+        subject = capitalizeFirstLetter(subject);
         if (!students.containsKey(name)) {
             throw new Exception("Student not found");
         }
@@ -38,6 +55,7 @@ public class GradeBook {
     }
 
     public float getGPA(String name) throws Exception {
+        name = capitalizeFirstLetter(name);
         if (!students.containsKey(name)) {
             throw new Exception("Student not found");
         }
@@ -45,6 +63,7 @@ public class GradeBook {
     }
 
     public String getLetterGrade(String name) throws Exception {
+        name = capitalizeFirstLetter(name);
         if (!students.containsKey(name)) {
             throw new Exception("Student not found");
         }
